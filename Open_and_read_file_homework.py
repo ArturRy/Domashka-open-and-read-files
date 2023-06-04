38c846d7e94a14323c7b4e2a8013b3a2730a576f
Artur <mini--me@mail.ru>
2023-06-05 00:08:14 +0300

    it`s alife_2

--- Open_and_read_file_homework.py
+++ Open_and_read_file_homework.py
@@ -1,39 +1,39 @@
-# import json
-# from pprint import pprint
-#
-# with open('recipes.txt', 'r', encoding='utf-8') as file:
-#     cook_book = {}
-#     for dishes in file:
-#         number_ingredients = int(file.readline())
-#         ingredients = []
-#         for i in range(number_ingredients):
-#             name_ingredient, amount, measure = file.readline().strip().split(' | ')
-#             ingredients.append({
-#                 'ingredient_name': name_ingredient,
-#                 'quantity': amount,
-#                 'measure': measure
-#             })
-#         file.readline()
-#         cook_book[dishes.strip()] = ingredients
-#
-#
-# print(json.dumps(cook_book,indent=2,ensure_ascii=False))
-#
-# def get_shop_list_by_dishes(dishes, person_count):
-#     dish_list = {}
-#     for dish in dishes:
-#         if dish in cook_book:
-#             for ing in cook_book[dish]:
-#                 if ing['ingredient_name'] in dish_list:
-#                     dish_list[ing['ingredient_name']]['quantity'] += int(ing['quantity']) * person_count
-#                 else:
-#                     dish_list[ing['ingredient_name']] = {'quantity': int(ing['quantity']) * person_count,
-#                                                          'measure': ing['measure']}
-#
-#     print(json.dumps(dish_list, indent=2, ensure_ascii=False))
-#
-#
-# get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
+import json
+from pprint import pprint
+
+with open('recipes.txt', 'r', encoding='utf-8') as file:
+    cook_book = {}
+    for dishes in file:
+        number_ingredients = int(file.readline())
+        ingredients = []
+        for i in range(number_ingredients):
+            name_ingredient, amount, measure = file.readline().strip().split(' | ')
+            ingredients.append({
+                'ingredient_name': name_ingredient,
+                'quantity': amount,
+                'measure': measure
+            })
+        file.readline()
+        cook_book[dishes.strip()] = ingredients
+
+
+print(json.dumps(cook_book,indent=2,ensure_ascii=False))
+
+def get_shop_list_by_dishes(dishes, person_count):
+    dish_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ing in cook_book[dish]:
+                if ing['ingredient_name'] in dish_list:
+                    dish_list[ing['ingredient_name']]['quantity'] += int(ing['quantity']) * person_count
+                else:
+                    dish_list[ing['ingredient_name']] = {'quantity': int(ing['quantity']) * person_count,
+                                                         'measure': ing['measure']}
+
+    print(json.dumps(dish_list, indent=2, ensure_ascii=False))
+
+
+get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
 
 
 import os
